e26413f5a17222a883164f305f3b856cccce789a
Oleksandra Mala <oleksandra.s.mala@gmail.com>
2024-11-11 16:19:35 +0100

    first commit

---
+++ src/main/java/com/cimba/fermetation/FermetationApplication.java
@@ -0,0 +1,13 @@
+package com.cimba.fermetation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FermetationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FermetationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cimba/fermetation/FermetationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cimba.fermetation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FermetationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
